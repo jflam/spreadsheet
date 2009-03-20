fbf74d58c0dbf60c6cf54f97864671bc410012e0
John Lam <jflam@microsoft.com>
2009-03-19 17:59:41 -0700

    adding missing file

---
+++ Spreadsheet/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace Spreadsheet {
+    public class Extensions {
+        private ScriptEngine _engine;
+        private ScriptScope _scope;
+        public const string DEFAULT_CODE = @"
+def sum(*args):
+    sum = 0
+    for arg in args:
+        sum += arg
+    return sum
+";
+
+        public Extensions() {
+            InitializeScripts();
+        }
+
+        private void InitializeScripts() {
+            _engine = Python.CreateEngine();
+            _scope = _engine.Runtime.CreateScope();
+            Code = Load();
+            _engine.Execute(Code, _scope);
+        }
+
+        public object Execute(string code) {
+            Code = code;
+            return _engine.Execute(Code, _scope);
+        }
+
+        public string Load() {
+            return DEFAULT_CODE;
+        }
+
+        public void Save(string code) {
+        }
+
+        public string Code { get; set; }
+    }
+}
